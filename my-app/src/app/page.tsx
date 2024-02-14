5d658bf8d53336cd9e314bea02451fa86b59ee4e
Phal Nhok <phal@xtricate.com>
2024-02-14 16:59:48 +0700

    update main nav

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,26 @@
-import Image from "next/image";
+import CustomLink from "@/components/ui/custom-link";
 
 export default function Home() {
   return (
-
-    <main>home</main>
-  );
+    <div className="space-y-2">
+      <h1 className="text-3xl font-bold">NextAuth.js Example</h1>
+      <p>
+        This is an example site to demonstrate how to use{" "}
+        <CustomLink href="https://nextjs.authjs.dev">NextAuth.js</CustomLink>{" "}
+        for authentication. Check out the{" "}
+        <CustomLink href="/server-example" className="underline">
+          Server
+        </CustomLink>{" "}
+        and the{" "}
+        <CustomLink href="/client-example" className="underline">
+          Client
+        </CustomLink>{" "}
+        examples to see how to secure pages and get session data.
+      </p>
+      <p>
+        Current{" "}
+        <CustomLink href="https://nextjs.authjs.dev">NextAuth.js</CustomLink>{" "}
+      </p>
+    </div>
+  )
 }
